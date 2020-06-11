795da19e9c343adbbaa7f6f28a165d33fed025d2
Keith Bentrup <keith@keithbentrup.com>
2020-06-11 08:01:10 -0400

    test

--- test.sh
+++ test.sh
@@ -10,6 +10,5 @@ LC_CTYPE $LC_CTYPE
 
 # rand_subdomain_suffix=$(LC_ALL=C tr -dc 'a-z' < /dev/random | fold -w 4 | head -n 1)
 ls -al /dev/random
-head -c 500 /dev/random | LC_ALL=C tr -dc 'a-z'
-head -c 500 /dev/urandom | LC_ALL=C tr -dc 'a-z'
+head -c 500 /dev/random | LC_ALL=C tr -dc 'a-z' | fold -w 4
 
