3d121af1a95564235645ddc4ef1f16e6c559b5dd
Keith Bentrup <keith@keithbentrup.com>
2020-06-11 14:26:59 -0400

    yes

--- test.sh
+++ test.sh
@@ -4,5 +4,4 @@ set -e
 
 echo "ARGH!"
 #rand_subdomain_suffix="$(base64 < /dev/random | LC_ALL=C tr '[:upper:]' '[:lower:]' | head -c 4)"
-echo "y" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
-
+yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
