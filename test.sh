3a160b2fb0fd0a2605d3792b10c64766e1284792
Keith Bentrup <keith@keithbentrup.com>
2020-06-11 10:19:55 -0400

    test

--- test.sh
+++ test.sh
@@ -6,5 +6,6 @@ trap - SIGPIPE
 
 echo "ARGH!"
 trap - SIGPIPE
-rand_subdomain_suffix="$(base64 < /dev/random | LC_ALL=C tr '[:upper:]' '[:lower:]' | head -c 4)"
+#rand_subdomain_suffix="$(base64 < /dev/random | LC_ALL=C tr '[:upper:]' '[:lower:]' | head -c 4)"
+rand_subdomain_suffix="$(head /dev/random | LC_ALL=C tr -dc 'a-z' | head -c 4)"
 echo "$rand_subdomain_suffix"
