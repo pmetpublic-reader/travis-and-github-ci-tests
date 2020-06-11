2ae2c360fa20a7b2f13107c60fac786186d8f538
Keith Bentrup <keith@keithbentrup.com>
2020-06-11 10:47:02 -0400

    test

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@ trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit
 [[ $debug ]] && set -x
 
 echo "ARGH!"
-trap - SIGPIPE
 #rand_subdomain_suffix="$(base64 < /dev/random | LC_ALL=C tr '[:upper:]' '[:lower:]' | head -c 4)"
 rand_subdomain_suffix="$(head /dev/random | LC_ALL=C tr -dc 'a-z' | head -c 4)"
 echo "$rand_subdomain_suffix"
