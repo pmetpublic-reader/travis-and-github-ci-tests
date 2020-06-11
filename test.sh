bae95da07f463f3d2e692a757ffa45e9c62f94c8
Keith Bentrup <keith@keithbentrup.com>
2020-06-11 09:28:21 -0400

    test

--- test.sh
+++ test.sh
@@ -4,13 +4,8 @@ trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit
 trap - SIGPIPE
 [[ $debug ]] && set -x
 
-echo "
-LANG $LANG
-LC_ALL $LC_ALL
-LC_CTYPE $LC_CTYPE
-"
+echo "reset sigpipe"
 
 # rand_subdomain_suffix=$(LC_ALL=C tr -dc 'a-z' < /dev/random | fold -w 4 | head -n 1)
 ls -al /dev/random
 head /dev/random | LC_ALL=C tr -dc 'a-z' | head -c 4
-
